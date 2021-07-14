0f9522d687845175a2a7bb069f02052308f9955a
AaronKilleen <AKilleen90@gmail.com>
2021-07-14 07:58:35 -0500

    Update project0 sql commands.sql

--- src/main/resources/project0 sql commands.sql
+++ src/main/resources/project0 sql commands.sql
@@ -40,7 +40,7 @@ INSERT INTO accounts (account_id, balance)
 	VALUES (123456789, 32543.21);
 
 INSERT INTO users (user_name, passwords, first_name, last_name, email, phone, ssn, street_address, city, state, zip, account_id, reg_approved, joint_approved, is_employee, is_admin) 
-	VALUES ('JohnQPublic', 'password', 'John', 'Customer', 'john@customer.com', 1112223333, 111223333, '100 John Street', 'Public City', 'IN', 11111, 123456789, true, true, false, false);
+	VALUES ('JohnQPublic', 'password', 'John', 'Public', 'john@customer.com', 1112223333, 111223333, '100 John Street', 'Public City', 'IN', 11111, 123456789, true, true, false, false);
 
 INSERT INTO accounts (account_id, balance)
 	VALUES (987654321, 11234.56);
